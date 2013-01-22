80d84c52acbb9d9bafcec544da71f50ce6889935
Bryan Hatton <bhatton@bhatton-ld.linkedin.biz>
2013-01-21 22:30:48 -0800

    In process of converting application to provide JSON responses to GET request Added toJSON() functions in model for RESTful API Minor updates to main.css (responsive design & misc bug fixes)

--- eatinPlay/app/models/Caterer.java
+++ eatinPlay/app/models/Caterer.java
@@ -1,37 +1,98 @@
-package models;
+package com.linkedin.eatin.model;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.sql.Connection;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.json.JSONObject;
 
 public class Caterer {
-	public String name;
-	public String type;
-	public int id;
-	public String image_url;
+	private Integer id;
+	private String name;
+	private Integer numLikes = 0;
+	private Integer numRatings = 1;
+	private String imageUrl;
+	private String foodType;
 	
-	public Caterer (String n, String t, int i, String iu) {
-		name = n;
-		type = t;
-		id = i;
-		image_url = iu;
+	private List<FoodItem> foodList;
+	private List<Comment> commentList;
+	
+	public Integer getId() { return id; }
+	public String getName() { return name; }
+	public Integer getNumLikes() { return numLikes; }
+	public Integer getNumRatings() { return numRatings; }
+	public Double getLikeRatio() { return (double) numLikes / (double) numRatings; }
+	public String getImageUrl() { return imageUrl; }
+	public String getFoodType() { return foodType; }
+	public List<Comment> getCommentList() { return commentList; }
+	
+	public void setId(Integer id) { this.id = id; }
+	public void setName(String name) { this.name = name; }
+	public void setNumLikes(Integer numLikes) { this.numLikes = numLikes; }
+	public void setNumRatings(Integer numRatings) { this.numRatings = numRatings; }
+	public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
+	public void setFoodType(String foodType) { this.foodType = foodType; }
+	
+	public Caterer(Integer id, String name, String imageUrl, String foodType) {
+		super();
+		
+		this.id = id;
+		this.name = name;
+		this.imageUrl = imageUrl;
+		this.foodType = foodType;
+		
+		this.foodList = new LinkedList<FoodItem>();
+		this.commentList = new LinkedList<Comment>();
 	}
 	
-	public Caterer(Connection connection, int i) {
-		try {
-			Statement statement = connection.createStatement();
-			String sql = "select Name, Type, ImgURL from caterer where ID = " + i + ";";
-			ResultSet result = statement.executeQuery(sql);
-			result.next();
-			name = result.getString(1);
-			type = result.getString(2);
-			id = i;
-			image_url = result.getString(3);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public void addFoodItem(FoodItem item) {
+		if (findFoodItem(item.getId()) == null) {
+			foodList.add(item);
+			updateRatings();
 		}
+	}
+	
+	public FoodItem findFoodItem(int id) {
+		for (FoodItem item : foodList) {
+			if (item.getId() == id)
+				return item;
+		}
+		return null;
+	}
+	
+	public void updateRatings() {
+		numLikes = 0;
+		numRatings = 0;
+		for (FoodItem item : foodList) {
+			numRatings += item.getNumRatings();
+			numLikes += item.getNumLikes();
+		}
+	}
+	
+	public void addComment(String text) {
+		commentList.add(new Comment(null, text, "Anonymous", new Date()));
+	}
+
+	public HashMap<String, Object> toJSON(){
+		HashMap<String, Object> mapper = new HashMap<String, Object>();
+		mapper.put("totalLikes", numLikes);
+		mapper.put("totalRatings", totalRatings);
+		mapper.put("catererId", id);
+		mapper.put("caterer", name);
+		mapper.put("foodType", foodType);
+		mapper.put("imageUrl", imageUrl);
+
+		List<Object> listOfItems = new List<Object>();
+				
+		for (int i = 0; i < commentList.size() ; i++ ) {
+			listOfItems.add( commentList.get(i).toJSON() );
+		}
+		mapper.put("comments", listOfItems);
+
+		return mapper;
+	}
+	
+	public void fromJSON(JSONObject json) {
+		
 	}
 }
