d7082db24c79892d738e12d07c30226940d74742
Jintae Jung <jjung@jjung-mn1.linkedin.biz>
2013-02-20 20:27:27 -0800

    removin' caterererererereres

--- webclient/public/javascripts/admin.js
+++ webclient/public/javascripts/admin.js
@@ -2,3 +2,9 @@ function publishAlert()
 {
 	alert("Publishing the menu was a success! :)");
 }
+
+function removeCaterer(rowId)
+{
+	var row = document.getElementById(rowId);
+	row.parentElement.removeChild(row); 
+}
